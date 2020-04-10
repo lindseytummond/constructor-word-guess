ad1b1e460f57440849119125108bd659b831ba66
lindsey tummond <lindseytummond@gmail.com>
2020-04-10 14:00:28 -0700

    linking npm and word to index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// dependency for inquirer npm package
+// npm package
 var inquirer = require("inquirer");
+
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+var GuessWord = require("./word.js")
+
+// Randomly selects a word and uses the Word constructor to store it
+
+
+
+
+// Prompts the user for each guess and keeps track of the user's remaining guesses
 
