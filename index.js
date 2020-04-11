b726af265d5bbee1abffce56e5f41533b62774ea
lindsey tummond <lindseytummond@gmail.com>
2020-04-10 17:50:13 -0700

    coffee and drinks and init function

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ var inquirer = require("inquirer");
 var GuessWord = require("./word.js")
 
 // Randomly selects a word and uses the Word constructor to store it
-
-
+var coffeeDrinks = ['affogato', 'americano', 'latte', 'cappuccino', 'espresso', 
+'frappe', 'macchiato', 'lungo', 'ristretto', 'mocha'];
 
 
 // Prompts the user for each guess and keeps track of the user's remaining guesses
+
+var guesses;
+var wordSelected;  //pickedWords
+var word;
+var wordPicked; //pickedWord
+
+function init() {
+    wordSelected = [];
+    console.log("Welcome to the Coffee Drink Word Guess Game!");
+    console.log("------------------------------------------");
+    playGame();
+  }
 
